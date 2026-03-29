b31bb4dbabdb6a9c611e25e2b2202784a37890d5
Felix <zargishiyev@gmail.com>
2026-03-29 14:32:24 +0300

    add environment config loading

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+
+	"cyber-defender-bot-tg/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println("telegram token loaded:", cfg.TelegramBotToken != "")
+	fmt.Println("virustotal api key loaded:", cfg.VirusTotalAPIKey != "")
+	fmt.Println("max file size bytes:", cfg.MaxFileSizeBytes)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cyber-defender-bot-tg
 
 go 1.25.1
+
+require github.com/joho/godotenv v1.5.1
